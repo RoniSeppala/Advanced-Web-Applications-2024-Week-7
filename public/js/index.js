145a23c7755a61f899cdb4d43b14ff796ccee210
Roni Seppälä <roni.o.seppala@gmail.com>
2025-01-06 18:30:05 +0200

    feat: add logout button and functionality to clear token and redirect to login

--- public/js/index.js
+++ public/js/index.js
@@ -10,3 +10,8 @@ fetch('http://localhost:3000/api/private', {
 .then(response => response.json())
 .then(data => console.log(data))
 .catch(error => console.error('Error:', error));
+
+document.getElementById('logout').addEventListener('click', () => {
+    localStorage.removeItem('token');
+    window.location.href = '/login.html';
+});
